5f27e77d5e93d399d41061e201a47f3f43f59469
AerhartOne <AerhartOne@gmail.com>
2019-05-01 14:33:23 +0800

    Added elements to MainDashboard.
    - Added Logout, Settings buttons.
    - Adjusted colours and layout of various elements.
    
    Signed-off-by: AerhartOne <AerhartOne@gmail.com>

--- src/pages/main-dashboard.js
+++ src/pages/main-dashboard.js
@@ -16,21 +16,33 @@ export default class MainDashboard extends React.Component{
     return (
       <>
       <div className='container-fluid d-flex flex-row justify-content-center align-items-center w-100 main-dashboard-container px-0'>
-        <div className='col-3 px-0 py-5 mx-0 sidebar h-100'>
-          <ul className='px-0 my-0 w-100'>
-            <li className='map-point-li w-100 py-3 px-3 my-1'>Test Location 1</li>
-            <li className='map-point-li w-100 py-3 px-3 my-1'>Test Location 2</li>
-            <li className='map-point-li w-100 py-3 px-3 my-1'>Test Location 3</li>
-            <li className='map-point-li w-100 py-3 px-3 my-1'>Test Location 4</li>
-          </ul>
+        <div className='col-3 px-0 py-0 mx-0 sidebar h-100'>
+          <div className='container-fluid d-flex flex-row text-center py-3 sidebar-top-panel'>
+            <div className='col'>
+              <button className='btn btn-lg w-100 btn-danger'>Log Out</button>
+            </div>
+            <div className='col'>
+              <button className='btn btn-lg w-100 btn-info'>Settings</button>
+            </div>
+          </div>
+          <div>
+            <h2 className='display-2 text-center py-3 my-0'>Saved Map Points</h2>
+            <ul className='px-0 my-0 w-100'>
+              <li className='map-point-li w-100 py-3 px-3 my-1'>Test Location 1</li>
+              <li className='map-point-li w-100 py-3 px-3 my-1'>Test Location 2</li>
+              <li className='map-point-li w-100 py-3 px-3 my-1'>Test Location 3</li>
+              <li className='map-point-li w-100 py-3 px-3 my-1'>Test Location 4</li>
+            </ul>
+          </div>
         </div>
         <div className='col-9 px-5 mx-0 h-100 d-flex flex-column justify-content-center align-items-center text-center'>
-            <h1 className='display-1 my-5 py-0'>Pick a location.</h1>
+            <h1 className='display-1 my-5 py-0 '>Pick a location.</h1>
 
-            
+
 
             <button className='btn btn-lg btn-warning my-5 py-3 w-50 save-mappoint-button'>Save MapPoint</button>
-            <div className='mapbox-embed w-75 h-50 text-center'> Mapbox goes here </div>
+
+            <div className='mapbox-embed w-75 h-50 text-center my-5'> Mapbox goes here </div>
         </div>
       </div>
       </>
