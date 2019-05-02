15ed7160134273460f19bb67fb24e11d67a11d50
AerhartOne <AerhartOne@gmail.com>
2019-05-02 15:21:07 +0800

    Added Mapbox Map embed
    - Added Mapbox Map with centre marker and coordinate readout
    
    Signed-off-by: AerhartOne <AerhartOne@gmail.com>

--- src/pages/main-dashboard.js
+++ src/pages/main-dashboard.js
@@ -2,17 +2,58 @@ import React from 'react';
 import '../css/main.css';
 import '../css/main-dashboard.css';
 import BasicModal from '../containers/basic-modal'
+import ReactMapBoxGL, {Layer, Feature, Map} from 'react-mapbox-gl'
+import mapboxGL from 'mapbox-gl';
+
+mapboxGL.accessToken = 'pk.eyJ1Ijoic3V6dWtpc3RldmVuIiwiYSI6ImNqdWpwcDhhYzFuczE0ZXAzamNkMWpvd2sifQ.PAW2yuz30KwTEL983iIN_g';
 
 export default class MainDashboard extends React.Component{
   constructor(props) {
     super(props)
 
     this.state = {
-      mapPoints: []
+      mapPoints: [],
+      mapCenter: [0,0],
+      zoom: 1
     }
+
+    this.onMapClick = this.onMapClick.bind(this)
+  }
+
+  componentDidMount() {
+    this.setupMap()
+  }
+
+  setupMap() {
+    const map = new mapboxGL.Map({
+      container: this.mapContainer,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: this.state.mapCenter
+    })
+
+    const centerMarker = new mapboxGL.Marker({
+      draggable: false
+    }).setLngLat(this.state.mapCenter).addTo(map)
+
+    map.on('move', () => {
+      const { lat, lng } = map.getCenter();
+      centerMarker.setLngLat([lng, lat])
+      this.setState({
+        mapCenter: [lat.toFixed(4), lng.toFixed(4)],
+        zoom: map.getZoom().toFixed(2)
+      })
+    })
+
+  }
+
+  onMapClick(e) {
+    this.setState({
+      mapCenter: e.lnglat
+    })
   }
 
   render(){
+    const Map = ReactMapBoxGL({accessToken:'pk.eyJ1Ijoic3V6dWtpc3RldmVuIiwiYSI6ImNqdWpwcDhhYzFuczE0ZXAzamNkMWpvd2sifQ.PAW2yuz30KwTEL983iIN_g'})
     return (
       <>
       <div className='container-fluid d-flex flex-row justify-content-center align-items-center w-100 main-dashboard-container px-0'>
@@ -36,13 +77,20 @@ export default class MainDashboard extends React.Component{
           </div>
         </div>
         <div className='col-9 px-5 mx-0 h-100 d-flex flex-column justify-content-center align-items-center text-center'>
-            <h1 className='display-1 my-5 py-0 '>Pick a location.</h1>
+          <h1 className='display-1 my-5 py-0 '>Pick a location.</h1>
+          <p> Latitude: {this.state.mapCenter[0]} </p>
+          <p> Longitude: {this.state.mapCenter[1]} </p>
 
+          <button className='btn btn-lg btn-warning my-5 py-3 w-50 save-mappoint-button'>Save MapPoint</button>
 
+          <div ref={e1 => this.mapContainer = e1} className='mapbox-embed my-5 w-75 h-50' />
 
-            <button className='btn btn-lg btn-warning my-5 py-3 w-50 save-mappoint-button'>Save MapPoint</button>
+          {/* <Map style="mapbox://styles/mapbox/streets-v11" containerStyle={{width: "75%", height: "60vh"}} center={this.state.mapCenter} className='mapbox-embed my-5'>
+            <Layer type="symbol" id="marker" layout={{ "icon-image": "marker-15" }}>
+              <Feature coordinates={this.state.mapCenter}/>
+            </Layer>
+          </Map> */}
 
-            <div className='mapbox-embed w-75 h-50 text-center my-5'> Mapbox goes here </div>
         </div>
       </div>
       </>
